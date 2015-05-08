f7daaac1898900972713e9963873c9028781830b
augustrush <liupingwei30@gmail.com>
2015-05-08 12:31:54 +0800

    add gif

---
+++ ARSegmentPager/WebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewController.h
+//  ARSegmentPager
+//
+//  Created by August on 15/5/7.
+//  Copyright (c) 2015年 August. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+
+@end
---
+++ ARSegmentPager/WebViewController.m
@@ -0,0 +1,37 @@
+//
+//  WebViewController.m
+//  ARSegmentPager
+//
+//  Created by August on 15/5/7.
+//  Copyright (c) 2015年 August. All rights reserved.
+//
+
+#import "WebViewController.h"
+
+@interface WebViewController ()
+
+@end
+
+@implementation WebViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
